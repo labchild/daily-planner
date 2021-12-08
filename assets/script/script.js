d71d57209ec320d8c06e114386faacb7f0540389
l bates childs <labchilds@gmail.com>
2021-12-08 18:23:37 -0500

    added page load function – date and stored tasks

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,6 @@
 var dateEl = $('#currentDay');
+var tasks = [];
 // var scheduleEl = $('#schedule');
-
-// add todays date to page
-dateEl.text(moment().format("dddd, MMMM Do YYYY"));
 
 // edit task 
 function handleEditTask() {
@@ -11,7 +9,7 @@ function handleEditTask() {
 
     // change p to textarea for input, focus on text area
     $(this).replaceWith(taskInput);
-    taskInput.trigger("focus"); 
+    taskInput.trigger("focus");
 };
 
 // save changes to local storage
@@ -19,27 +17,35 @@ function handleSaveTask() {
     var taskInput = $(this).siblings(".task");
     var hourId = $(this).closest(".time-block").attr("id");
     var taskText = $("<p>")
-    .addClass("col description border border-dark")
-    .text(taskInput.val().trim());
+        .addClass("col description border border-dark")
+        .text(taskInput.val());
+    var taskObj = {
+        time: hourId,
+        description: taskInput.val().trim(),
+        date: dateEl.text()
+    }
 
+    // send task obj to tasks arr
+    tasks = tasks.push(taskObj);
+    console.log(tasks);
     // convert back to p
     taskInput.replaceWith(taskText);
+    updateColor();
 
     // save new task description to local storage
-    localStorage.setItem(hourId, taskInput.val());
+    localStorage.setItem("tasks", tasks);
 };
 
 
 // audit div time
 function updateColor() {
-    // timefromtask is now.diff(hourid)
+
     var now = JSON.parse(moment().format("HH"));
-    console.log(now);
-    
-    $(".time-block").each( function() {
-        
+
+    $(".time-block").each(function () {
+
         var hourId = $(this).attr("id");
-        
+
         // if hour has elapsed, red backround for description
         if (hourId == now) {
             $(this).children(".description").addClass("present");
@@ -52,11 +58,11 @@ function updateColor() {
         if (hourId > now) {
             $(this).children(".description").addClass("future");
         }
-     })
+    })
 }
 
-function updateTimer () {
-    // get min of current time
+function updateTimer() {
+    // get min of current time, if on the hour, color code
     var min = moment().format('mm');
     if (min == 0) {
         updateColor();
@@ -65,17 +71,32 @@ function updateTimer () {
 
 
 // on page load
-    // populate todays date
-    // populate tasks from local storage
+function pageLoad() {
+    // add todays date to page
+    dateEl.text(moment().format("dddd, MMMM Do YYYY"));
 
-// local storage
+    // populate tasks from local storage
+    handleStoredTasks();
+    updateColor();
+}
+
+
+
+// local storage to page
+function handleStoredTasks() {
+    console.log("hi!");
     // clear storage every 24 hours (9am?)
-    // on save btn click, push associated task to local storage
+    // if today is after date value in obj then remove
+    // else print to page on load, push to tasks array
+
+}
+
+
 
 
 // listeners
-// $("document").ready(pageLoad);
+$("document").ready(pageLoad);
 $(".time-block").on("click", ".description", handleEditTask);
 $(".time-block").on("click", ".saveBtn", handleSaveTask);
-updateColor();
+
 setInterval(updateTimer, 1000 * 10)
